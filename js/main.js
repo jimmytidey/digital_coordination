338b3e6054b53cfac5dc584444b40a59297a19b7
Jimmy Tidey <jimmytidey@gmail.com>
2015-09-30 19:05:56 +0100

    adding icons

--- js/main.js
+++ js/main.js
@@ -3,18 +3,51 @@ var $grid = $('.grid').isotope({
   layoutMode: 'fitRows',    
   getSortData: {
       name: '.name',
-      organisation-structure: '.organisation-structure', 
-      exchange-mechanism: '.exchange-mechanism', 
-      resource-structure: '.resource-structure',
-      resource-type: '.resource-type' 
+      organisation_structure: function( itemElem ) {
+       
+        var org_type = $( itemElem ).find('.organisation-structure').attr('data-organisation-structure'); 
+        console.log(org_type);
+
+        return parseFloat(org_type);
+      }, 
+      exchange_mechanism: '.exchange-mechanism', 
+      resource_structure: '.resource-structure',
+      resource_type: '.resource-type' 
   }
 });
 
 
+$('.name_btn').click(function(){
+    $grid.isotope({ 
+        sortBy: 'name' 
+    });
+});
 
-$grid.isotope({ 
-    sortBy: 'name' 
-})
+$('.organisation_structure_btn').click(function(){
+    $grid.isotope({ 
+        sortBy: 'organisation_structure' 
+    });
+});
+
+$('.exchange_mechanism_btn').click(function(){ 
+    $grid.isotope({ 
+        sortBy: 'exchange_mechanism' 
+    });
+});
+
+$('.resource_structure_btn').click(function(){
+    $grid.isotope({ 
+        sortBy: 'resource_structure' 
+    });
+});
+
+$('.resource_type_btn').click(function(){ 
+    $grid.isotope({ 
+        sortBy: 'resource_type' 
+    });
+});
+
+
 
 
   
