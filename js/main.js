b2e414a6c06eec32b2c3937f9273ed3d46b0bed6
jimmytidey <jimmytidey@gmail.com>
2015-10-10 20:55:31 +0100

    changing column css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+
+/*
 var $grid = $('.grid').isotope({
   itemSelector: '.grid-item',
-  layoutMode: 'fitRows',    
+  layoutMode: 'cellsByColumn',
+  cellsByColumn: {
+    columnWidth: 110,
+    rowHeight: 110
+  },
   getSortData: {
       name: '.name',
       organisation_structure: function( itemElem ) {
@@ -21,30 +27,56 @@ var $grid = $('.grid').isotope({
       }
   }
 });
+*/
 
-
-$('.name_btn').click(function(){
-    $grid.isotope({ 
-        sortBy: 'name' 
-    });
-});
-
-$('.organisation_structure_btn').click(function(){
-    $grid.isotope({ 
-        sortBy: 'organisation_structure' 
-    });
-});
 
 $('.exchange_mechanism_btn').click(function(){ 
-    $grid.isotope({ 
-        sortBy: 'exchange_mechanism' 
-    });
+  
+  $('.grid-item').fadeOut();
+
+
+  setTimeout(function(){ 
+    $('.grid-item').each(function(key,elem){ 
+      var type = $(elem).find('.exchange-mechanism').data("exchange-mechanism");
+      $(elem).appendTo('.category-column-'+type);
+      $(elem).fadeIn({queue:true});
+    });    
+  }, 380);
+
 });
 
-$('.resource_structure_btn').click(function(){
-    $grid.isotope({ 
-        sortBy: 'resource_structure' 
-    });
+
+$('.resource_structure_btn').click(function(){ 
+  
+  $('.grid-item').fadeOut();
+
+
+  setTimeout(function(){ 
+    $('.grid-item').each(function(key,elem){ 
+      var type = $(elem).find('.resource-structure').data("resource-structure");
+      $(elem).appendTo('.category-column-'+type);
+      $(elem).fadeIn({queue:true});
+    });    
+  }, 380);
+
+});
+
+
+$('.fractional_capacity_btn').click(function(){
+
+  $('.grid-item').fadeOut();
+
+
+  setTimeout(function(){ 
+    $('.grid-item').each(function(key,elem){ 
+      var type = $(elem).find('.fractional_capacity').data("fractional-capacity");
+      console.log(type);
+      $(elem).appendTo('.category-column-'+type);
+      $(elem).fadeIn({queue:true});
+    });    
+  }, 380);
+
+
 });
 
 $('.resource_type_btn').click(function(){ 
